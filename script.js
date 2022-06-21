4f012541786997643b8960bd212eb79a57678bf1
semapavel <semapavel89@gmail.com>
2022-06-21 20:09:50 +0500

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function min(a, b){
-	return a<b?a:b;
+let x=prompt('Введите число x','');
+let n=prompt('Введите число n','');
+if (n<1){
+	alert('Введите натуральное число n')
+} else {
+	alert(pow(x,n));
 }
-alert(min(3,10));
+
+function pow(x,n){
+	let sum=x;
+	for(let i=1;i<=n;i++){
+		sum*=x;
+	}
+	return sum;
+}
