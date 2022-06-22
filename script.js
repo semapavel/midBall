1636f134ec59775a532cf9e5edf8f7817c17d771
semapavel <semapavel89@gmail.com>
2022-06-22 16:03:38 +0500

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
- let schedule = {};
-// alert( isEmpty(schedule) );
-schedule["8:30"] = "get up";
-alert( isEmpty(schedule) );
-
-function isEmpty(obj) {
-	for (let key of obj) {
-		return false;
-	}
-	return true;
+let salaries = {
+	John: 100,
+	Ann: 160,
+	Pete: 130
 }
+let sum=0;
+for (let key in salaries){
+	sum+=salaries[key]
+}
+alert(sum);
