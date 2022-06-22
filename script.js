dfe1c4090916c9a3455581504ad7cafa301627df
semapavel <semapavel89@gmail.com>
2022-06-22 23:25:42 +0500

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function checkSpam(str){
-	let lowStr=str.toLowerCase();
-	return lowStr.includes('viagra')||lowStr.includes('xxx')
+function truncate(str, maxlength){
+	if(str.length>maxlength){
+		return str.slice(0,maxlength-1)+'...'
+	} else{
+		return str
+	}
 }
 
-alert(checkSpam('buy ViAgRA now'))
-alert(checkSpam('free xxxxx'))
-alert(checkSpam("innocent rabbit"))
+alert(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
+alert(truncate("Всем привет!", 20));
