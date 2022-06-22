31ac31e38dae99175a7e0281f8bc566f85dac875
semapavel <semapavel89@gmail.com>
2022-06-22 16:40:46 +0500

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let salaries = {
-	John: 100,
-	Ann: 160,
-	Pete: 130
-}
-let sum=0;
-for (let key in salaries){
-	sum+=salaries[key]
-}
-alert(sum);
+let menu = {
+	width: 200,
+	height: 300,
+	title: "My menu"
+ };
+ 
+
+multiplyNumeric(menu);
+console.log(menu);
+
+ function multiplyNumeric(obj){
+	for(let key in obj){
+		if (typeof obj[key]=='number'){
+			obj[key]*=2;
+		}
+	}
+ }
