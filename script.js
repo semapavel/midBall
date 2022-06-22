783fd783717b06ba44a149edd9b1fb5b0674dd60
semapavel <semapavel89@gmail.com>
2022-06-23 01:13:47 +0500

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-let arr = [5, 2, 1, -10, 8];
-arr.sort((a,b)=>b-a); // ... код для сортировки по убыванию
-alert( arr ); // 8, 5, 2, 1, -10
+function copySorted(arr){
+	return arr.slice().sort();
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert( sorted ); // CSS, HTML, JavaScript
+alert( arr ); // HTML, JavaScript, CSS (без изменений)
