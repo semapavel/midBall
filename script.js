a6756cff4b7f6ae9a78e30ae43aa31f0b4b99e90
semapavel <semapavel89@gmail.com>
2022-06-22 23:14:15 +0500

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function ucFirst(str){
-	if (!str) return str;
-	return str[0].toUpperCase() + str.slice(1);
+function checkSpam(str){
+	let lowStr=str.toLowerCase();
+	return lowStr.includes('viagra')||lowStr.includes('xxx')
 }
-alert(ucFirst(""));
+
+alert(checkSpam('buy ViAgRA now'))
+alert(checkSpam('free xxxxx'))
+alert(checkSpam("innocent rabbit"))
