f82fdf549b891a67aeb02dea93c0495042fe7c27
semapavel <semapavel89@gmail.com>
2022-06-23 00:21:40 +0500

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-function sumInput(){
-	let array=[];
-	do {
-		num=+prompt('Введите число:','')
-		if(!isFinite(num)||num==''||num==null){
-			break
-		} else {
-			array.push(num)
-		}
-	} while (true)
-	let sum=0;
-	for(let arr of array){
-		sum+=arr
+function getMaxSubSum(arr) {
+	let max = 0;
+	let sum = 0;
+	for(let num of arr){
+		sum+=num;
+		if(sum>max)max=sum;
+		if(sum<0) sum=0;
 	}
-	return sum
+	return max
 }
-alert(sumInput());
+
+alert( getMaxSubSum([-1, 2, 3, -9]) );
+alert( getMaxSubSum([-1, 2, 3, -9, 11]) );
+alert( getMaxSubSum([-2, -1, 1, 2]) );
+alert( getMaxSubSum([1, 2, 3]) );
+alert( getMaxSubSum([100, -9, 2, -3, 5]) );
