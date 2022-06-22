09bdaeb5239bca2d9b9e16124fcae230b8b8bb19
semapavel <semapavel89@gmail.com>
2022-06-22 15:30:35 +0500

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-let x=prompt('Введите число x','');
-let n=prompt('Введите число n','');
-if (n<1){
-	alert('Введите натуральное число n')
-} else {
-	alert(pow(x,n));
-}
-
-function pow(x,n){
-	let sum=x;
-	for(let i=1;i<=n;i++){
-		sum*=x;
-	}
-	return sum;
-}
+let user={};
+user.name='John';
+user.surname='Smith';
+user.name='Pete';
+delete user.name;
