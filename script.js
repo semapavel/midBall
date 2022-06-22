f05e10cea41c1328bcab62751f5953a46a152ab8
semapavel <semapavel89@gmail.com>
2022-06-23 01:07:17 +0500

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function filterRangeInPlace(arr, a, b){
-	for(i=0;i<arr.length;i++){
-		if (arr[i]<a||arr[i]>b){
-			arr.splice(i,1);
-			i--;
-		}
-	}
-	return arr
-}
-
-let arr = [5, 3, 8, 1];
-
-console.log(filterRangeInPlace(arr, 1, 4));
+let arr = [5, 2, 1, -10, 8];
+arr.sort((a,b)=>b-a); // ... код для сортировки по убыванию
+alert( arr ); // 8, 5, 2, 1, -10
