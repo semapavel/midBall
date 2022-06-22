e8677d721cb555f172317322e53a2d436509124d
semapavel <semapavel89@gmail.com>
2022-06-22 23:47:13 +0500

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function extractCurrencyValue(str){
-	return +str.slice(1)
-}
-
- alert( extractCurrencyValue('$120'));
+let styles=["Джаз","Блюз"];
+styles.push("Рок-н-ролл");
+styles[Math.floor((styles.length-1)/2)]="Классика";
+alert(styles.shift());
+styles.unshift("Рэп","Регги");
