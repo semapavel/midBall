91c11f4763320b0960ec24e61aa7277f846f3157
semapavel <semapavel89@gmail.com>
2022-06-23 01:46:49 +0500

    first commit

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
 
 let users = [ vasya, petya, masha ];
 
-let names =users.map(item=>item.name); /* ... ваш код */
+let usersMapped = users.map(
+	item=>({
+	fullName: `${item.name} ${item.surname}`,
+	id: item.id
+})) /* ... ваш код ... */
 
-alert( names ); // Вася, Петя, Маша
+/*
+usersMapped = [
+  { fullName: "Вася Пупкин", id: 1 },
+  { fullName: "Петя Иванов", id: 2 },
+  { fullName: "Маша Петрова", id: 3 }
+]
+*/
+
+alert( usersMapped[1].id ) // 1
+alert( usersMapped[1].fullName ) // Вася Пупкин
