65c6954d028c6337c07fcae335c87ce79386a205
semapavel <semapavel89@gmail.com>
2022-06-23 01:33:48 +0500

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-function copySorted(arr){
-	return arr.slice().sort();
+function Calculator(){
+	this.methods = {
+		"-": (a, b) => a - b,
+		"+": (a, b) => a + b
+	};
+	this.calculate = function(str){
+		let split = str.split(' '),
+		a = +split[0],
+		op = split[1],
+		b = +split[2]
+		if(!this.methods[op] || isNaN(a) || isNaN(b)){
+			return NaN;
+		}
+		return this.methods[op](a, b);
+	}
+	this.addMethod = function(name, func){
+		this.methods[name] = func;
+	};
 }
 
-let arr = ["HTML", "JavaScript", "CSS"];
+let powerCalc = new Calculator;
+powerCalc.addMethod("*", (a, b) => a * b);
+powerCalc.addMethod("/", (a, b) => a / b);
+powerCalc.addMethod("**", (a, b) => a ** b);
 
-let sorted = copySorted(arr);
-
-alert( sorted ); // CSS, HTML, JavaScript
-alert( arr ); // HTML, JavaScript, CSS (без изменений)
+let result = powerCalc.calculate("2 + 3");
+alert( result );
