a021feccd9646a3b31799b15a306ea44415e7c4e
semapavel <semapavel89@gmail.com>
2022-06-22 15:56:22 +0500

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let user={};
-user.name='John';
-user.surname='Smith';
-user.name='Pete';
-delete user.name;
+ let schedule = {};
+// alert( isEmpty(schedule) );
+schedule["8:30"] = "get up";
+alert( isEmpty(schedule) );
+
+function isEmpty(obj) {
+	for (let key of obj) {
+		return false;
+	}
+	return true;
+}
