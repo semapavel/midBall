44b34b0aa359437562abeb9c8ff7051353799754
semapavel <semapavel89@gmail.com>
2022-06-23 01:37:57 +0500

    first commit

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-function Calculator(){
-	this.methods = {
-		"-": (a, b) => a - b,
-		"+": (a, b) => a + b
-	};
-	this.calculate = function(str){
-		let split = str.split(' '),
-		a = +split[0],
-		op = split[1],
-		b = +split[2]
-		if(!this.methods[op] || isNaN(a) || isNaN(b)){
-			return NaN;
-		}
-		return this.methods[op](a, b);
-	}
-	this.addMethod = function(name, func){
-		this.methods[name] = func;
-	};
-}
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
 
-let powerCalc = new Calculator;
-powerCalc.addMethod("*", (a, b) => a * b);
-powerCalc.addMethod("/", (a, b) => a / b);
-powerCalc.addMethod("**", (a, b) => a ** b);
+let users = [ vasya, petya, masha ];
 
-let result = powerCalc.calculate("2 + 3");
-alert( result );
+let names =users.map(item=>item.name); /* ... ваш код */
+
+alert( names ); // Вася, Петя, Маша
