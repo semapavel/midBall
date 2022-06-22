79e7a387bc78c21606eb14bbeb341bab432aa26d
semapavel <semapavel89@gmail.com>
2022-06-22 23:28:41 +0500

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-function truncate(str, maxlength){
-	if(str.length>maxlength){
-		return str.slice(0,maxlength-1)+'...'
-	} else{
-		return str
-	}
+function extractCurrencyValue(str){
+	return +str.slice(1)
 }
 
-alert(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
-alert(truncate("Всем привет!", 20));
+ alert( extractCurrencyValue('$120'));
