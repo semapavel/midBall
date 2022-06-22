47fde971bd8eca6eefe7b149b45bdf340ba3f9b0
semapavel <semapavel89@gmail.com>
2022-06-23 02:18:32 +0500

    first commit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function shuffle(array){
-	for(let i = array.length - 1; i > 0; i--){
-		let j=Math.floor(Math.random()*(i+1));
-		[array[i], array[j]] = [array[j], array[i]];
-	}
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 29 };
+
+let arr = [ vasya, petya, masha ];
+
+alert( getAverageAge(arr) );
+
+function getAverageAge(users){
+	return users.reduce((sum,item)=>sum+item.age,0)/users.length
 }
-let arr = [1, 2, 3];
-shuffle(arr);
-alert(arr);
