c045948d2216ab6d17eb216a09682c11b969b6a2
semapavel <semapavel89@gmail.com>
2022-06-24 01:33:46 +0500

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-let user = {
-	name: "Василий Иванович",
-	age: 35
+let room = {
+	number: 23
  };
- let json=JSON.parse(JSON.stringify(user));
-console.log(json);
+ 
+ let meetup = {
+	title: "Совещание",
+	occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+	place: room
+ };
+ 
+ // цикличные ссылки
+ room.occupiedBy = meetup;
+ meetup.self = meetup;
+ 
+ alert( JSON.stringify(meetup, function replacer(key, value) {
+	return(key!=""&&value==meetup)?undefined:value;
+ }));
+ 
+ /* в результате должно быть:
+ {
+	"title":"Совещание",
+	"occupiedBy":[{"name":"Иванов"},{"name":"Петров"}],
+	"place":{"number":23}
+ }
+ */
