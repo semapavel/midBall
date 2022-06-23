16c0a37982b43adfa7fe460d99e40b57eb511699
semapavel <semapavel89@gmail.com>
2022-06-24 00:07:22 +0500

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 
-function getDateAgo(date, days){
-	let dates=new Date(date);
-	dates.setDate(date.getDate()-days);
-	 return dates.getDate();
+function getLastDayOfMonth(year, month){
+	return new Date(year,month+1,0).getDate()
 }
-let date = new Date(2015, 0, 2);
 
-alert( getDateAgo(date, 1) ); // 1, (1 Jan 2015)
-alert( getDateAgo(date, 2) ); // 31, (31 Dec 2014)
-alert( getDateAgo(date, 365) ); // 2, (2 Jan 2014)
+alert( getLastDayOfMonth(2012, 1) );
