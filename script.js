6477b48fa9fdc7288b881f51c5df10f3756d8a72
semapavel <semapavel89@gmail.com>
2022-06-23 23:34:58 +0500

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-function topSalary(salaries){
-	let maxName=null;
-	let maxSalary=0;
-	for(let [name,salary] of Object.entries(salaries)){
-		if (maxSalary<salary){
-			maxSalary=salary;
-			maxName=name;
-		}
-	}
-	return maxName;
-}
-let salaries = {
-	"John": 300,
-	"Pete": 300,
-	"Mary": 250
- };
- console.log(topSalary(salaries));
+date=new Date(2012,1,20,3,12)
+alert(date);
