c1532282e7fb88d421a677507e6cec9e58ed5413
semapavel <semapavel89@gmail.com>
2022-06-23 18:18:36 +0500

    first commit

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ let messages = [
 	{text: "See you soon", from: "Alice"}
 ];
 
-let read=new WeakSet();
-read.add(messages[0]);
-read.add(messages[1]);
-read.add(messages[0]);
-alert("Read message 0: " + read.has(messages[2]));
-messages.shift();
+let read = new WeakMap();
+read.set(messages[0], new Date());
+console.log(read);
