2fa5b4380d5c356a791a6717437ccaa94393d49e
semapavel <semapavel89@gmail.com>
2022-06-23 23:40:00 +0500

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-date=new Date(2012,1,20,3,12)
-alert(date);
+
+function getWeekDay(date){
+	let day=['ВС','ПН','ВТ','СР','ЧТ','ПТ','СБ'];
+	return day[date.getDay()]
+}
+let date = new Date(2012, 0, 3);  // 3 января 2012 года
+alert( getWeekDay(date) );        // нужно вывести "ВТ"
