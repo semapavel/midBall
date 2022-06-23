b05d69d090a2d7bb192b2c8c59cf5f65278c17a6
semapavel <semapavel89@gmail.com>
2022-06-23 15:50:26 +0500

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function unique(arr) {
-	return Array.from(new Set(arr))
+function aclean(arr) {
+	let map=new Map();
+	for(let item of arr){
+		let sort=item.toLowerCase().split('').sort().join('');
+		map.set(sort,item);
+	}
+	return Array.from(map.values())
 	/* ваш код */
  }
  
- let values = ["Hare", "Krishna", "Hare", "Krishna",
-	"Krishna", "Krishna", "Hare", "Hare", ":-O"
- ];
- 
- alert( unique(values) ); // Hare,Krishna,:-O
+ let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+ alert( aclean(arr) ); // "nap,teachers,ear" или "PAN,cheaters,era"
