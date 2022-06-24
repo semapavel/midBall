55ebe035e7ccf42b222f081600404a937e59bc36
semapavel <semapavel89@gmail.com>
2022-06-24 12:03:00 +0500

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function factorial(n){
-	return n==1?n:n*factorial(n-1);
-}
+function fib(n){
+	let num1=1;
+	let num2=1;
+	for(let i=3;i<=n;i++){
+		let sum=num1+num2;
+		num1=num2;
+		num2=sum;
+	}
+	return num2;
+ }
 
-alert(factorial(7));
+alert( fib(3) ); // 2
+alert( fib(7) ); // 13
+alert( fib(77) ); // 5527939700884757
