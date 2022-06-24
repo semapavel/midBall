672fc11185c81fc76ad55250313b4d73217c6e04
semapavel <semapavel89@gmail.com>
2022-06-24 13:45:44 +0500

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function sum(a){
-	return function(b){
-		return a+b;
+function inBetween(a,b){
+	return function(num){
+		return num>=a&&num<=b;
 	};
 }
 
-alert(sum(1)(2));
-alert(sum(5)(-1));
+function inArray(arr){
+	return function(num){
+		return arr.includes(num);
+	};
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+alert( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
+
+alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
