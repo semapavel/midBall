8af6eb612f4f1ed65614b85203de2a1432709e26
semapavel <semapavel89@gmail.com>
2022-06-24 18:26:11 +0500

    first commit

--- script.js
+++ script.js
@@ -1,36 +1,20 @@
-function f(a) {
-	console.log(a)
- }
- 
- // f1000 передаёт вызовы f максимум раз в 1000 мс
- let f1000 = throttle(f, 1000);
- 
-f1000(1); // показывает 1
-f1000(2); // (ограничение, 1000 мс ещё нет)
-f1000(3); // (ограничение, 1000 мс ещё нет)
- 
- // когда 1000 мс истекли ...
- // ...выводим 3, промежуточное значение 2 было проигнорировано
+let head = {
+	glasses: 1
+};
+let table = {
+	pen: 3,
+	__proto__: head
+};
+let bed = {
+	sheet: 1,
+	pillow: 2,
+	__proto__: table
+};
+let pockets = {
+	money: 2000,
+	__proto__: bed
+};
 
-function throttle(func, ms) {
-	let isThrottled = false,
-	savedArgs,
-	savedThis;
-	function wrapper() {
-		if (isThrottled) {
-			savedArgs = arguments;
-			savedThis = this;
-			return;
-		}
-		func.apply(this, arguments);
-		isThrottled = true;
-		setTimeout(function() {
-			isThrottled = false;
-			if (savedArgs) {
-				wrapper.apply(savedThis, savedArgs);
-				savedArgs = savedThis = null;
-			}
-		}, ms);
-	}
-	return wrapper;
-}
+alert( pockets.pen ); // 3
+alert( bed.glasses ); // 1
+alert( table.money ); // undefined
