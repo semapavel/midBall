f18372f87a3d3d657a5456999b431b2c3ab9e189
semapavel <semapavel89@gmail.com>
2022-06-24 16:29:39 +0500

    first commit

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-let f = debounce(alert, 1000);
+function f(a) {
+	console.log(a)
+ }
+ 
+ // f1000 передаёт вызовы f максимум раз в 1000 мс
+ let f1000 = throttle(f, 1000);
+ 
+f1000(1); // показывает 1
+f1000(2); // (ограничение, 1000 мс ещё нет)
+f1000(3); // (ограничение, 1000 мс ещё нет)
+ 
+ // когда 1000 мс истекли ...
+ // ...выводим 3, промежуточное значение 2 было проигнорировано
 
-f(1); // выполняется немедленно
-f(2); // проигнорирован
-
-setTimeout( () => f(3), 100); // проигнорирован (прошло только 100 мс)
-setTimeout( () => f(4), 1100); // выполняется
-setTimeout( () => f(5), 1500); // проигнорирован (прошло только 400 мс от последнего вызова)
-
-function debounce(f, ms){
-	let isCooldown=false;
-	return function(){
-		if(isCooldown) return;
-		f.apply(this, arguments);
-		isCooldown =true;
-		setTimeout(()=>isCooldown=false, ms);
-	};
+function throttle(func, ms) {
+	let isThrottled = false,
+	savedArgs,
+	savedThis;
+	function wrapper() {
+		if (isThrottled) {
+			savedArgs = arguments;
+			savedThis = this;
+			return;
+		}
+		func.apply(this, arguments);
+		isThrottled = true;
+		setTimeout(function() {
+			isThrottled = false;
+			if (savedArgs) {
+				wrapper.apply(savedThis, savedArgs);
+				savedArgs = savedThis = null;
+			}
+		}, ms);
+	}
+	return wrapper;
 }
