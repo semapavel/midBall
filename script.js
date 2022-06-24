d7d76683073874523b370c2b65159fcd55b13d30
semapavel <semapavel89@gmail.com>
2022-06-24 12:09:40 +0500

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-function fib(n){
-	let num1=1;
-	let num2=1;
-	for(let i=3;i<=n;i++){
-		let sum=num1+num2;
-		num1=num2;
-		num2=sum;
+let list = {
+	value: 1,
+	next: {
+	  value: 2,
+	  next: {
+		 value: 3,
+		 next: {
+			value: 4,
+			next: null
+		 }
+	  }
 	}
-	return num2;
- }
+ };
 
-alert( fib(3) ); // 2
-alert( fib(7) ); // 13
-alert( fib(77) ); // 5527939700884757
+// function printList(list){
+// 	let obj=list;
+// 	while(obj){
+// 		alert(obj.value);
+// 		obj=obj.next;
+// 	}
+// }
+
+function printList(list){
+	alert(list.value);
+	if(list.next){
+		printList(list.next);
+	}
+}
+
+printList(list);
