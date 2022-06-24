e5108ea7cd522a156587a8e63dd7e50d3e2d9d56
semapavel <semapavel89@gmail.com>
2022-06-24 15:41:55 +0500

    first commit

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-function sum(a){
-	let res=a;
-	function f(b){
-		res+=b;
-		return f;
-	}
-	f.toString=function(){
-		return res;
-	};
-	return f;
+// function printNumbers(from, to){
+// 	let i=from;
+// 	setTimeout(function repeat(){
+// 		alert(i);
+// 		if(i<to){
+// 			setTimeout(repeat,1000)
+// 		}
+// 		i++;
+// 	},1000)
+// }
+
+function printNumbers(from, to){
+	let i=from;
+	let timerId=setInterval(function (){
+		alert(i);
+		if(i==to){
+			clearInterval(timerId)
+		}
+		i++;
+	},1000)
 }
- 
- alert( sum(1)(2) ); // 3
- alert( sum(5)(-1)(2) ); // 6
- alert( sum(6)(-1)(-2)(-3) ); // 0
- alert( sum(0)(1)(2)(3)(4)(5) ); // 15
+printNumbers(4, 6)
