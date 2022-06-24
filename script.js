27d3983caa323682e93262190f49a16a0c8fa325
semapavel <semapavel89@gmail.com>
2022-06-24 11:54:53 +0500

    first commit

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-// function sumTo(n){
-// 	let sum=0;
-// 	for (let i = 1; i <= n; i++){
-// 		sum += i;
-// 	}
-// 	return sum;
-// }
-
-// function sumTo(n){
-// 	if(n==1){
-// 		return 1
-// 	}else{
-// 		return n + sumTo(n - 1);
-// 	}
-// }
-
-function sumTo(n){
-	return n*(n+1)/2;
+function factorial(n){
+	return n==1?n:n*factorial(n-1);
 }
 
-alert( sumTo(100) );
+alert(factorial(7));
