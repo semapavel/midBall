fdb65d5eebd8e118e9a05aa8ed919b9bc2d6cc8b
semapavel <semapavel89@gmail.com>
2022-06-24 15:01:14 +0500

    first commit

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-function makeCounter() {
-	let count = 0;
-	function counter(){
-		 return count++
+function sum(a){
+	let res=a;
+	function f(b){
+		res+=b;
+		return f;
 	}
-	counter.set=value=>count=value;
-	counter.decrease=()=>count--;
-	return counter;
+	f.toString=function(){
+		return res;
+	};
+	return f;
 }
  
-let counter = makeCounter();
- 
-alert( counter() ); // 0
-alert( counter() ); // 1
- 
-counter.set(10); // установить новое значение счётчика
- 
-alert( counter() ); // 10
- 
-counter.decrease(); // уменьшить значение счётчика на 1
- 
-alert( counter() ); // 10 (вместо 11)
+ alert( sum(1)(2) ); // 3
+ alert( sum(5)(-1)(2) ); // 6
+ alert( sum(6)(-1)(-2)(-3) ); // 0
+ alert( sum(0)(1)(2)(3)(4)(5) ); // 15
