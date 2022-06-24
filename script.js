ca0b00c825e79ed5bdadc68c6bc55532b03c5fa9
semapavel <semapavel89@gmail.com>
2022-06-24 19:25:56 +0500

    first commit

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function f() {
 	alert("Hello!");
- }
+}
  
 Function.prototype.defer=function(ms){
-	setTimeout(this,ms)
-}
+	let f = this;
+	return function(...args) {
+		setTimeout(() => f.apply(this, args), ms);
+	}}
 
- f.defer(1000); // выведет "Hello!" через 1 секунду
+function f(a, b) {
+	alert( a + b );
+	}
+	
+	f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
