f980081b310bd1f8a70df9f88c16e714fc6ad2f1
semapavel <semapavel89@gmail.com>
2022-06-24 13:36:33 +0500

    first commit

--- script.js
+++ script.js
@@ -1,35 +1,8 @@
-let list = {
-	value: 1,
-	next: {
-	  value: 2,
-	  next: {
-		 value: 3,
-		 next: {
-			value: 4,
-			next: null
-		 }
-	  }
-	}
- };
-
-function printReverseList(list){
-	let arr=[];
-	let obj=list;
- 
-	while (obj){
-		arr.push(obj.value);
-		obj=obj.next;
-	}
-	for(let i=arr.length-1;i>=0;i--){
-		alert(arr[i]);
-	}
+function sum(a){
+	return function(b){
+		return a+b;
+	};
 }
 
-// function printReverseList(list){
-// 	if(list.next){
-// 		printReverseList(list.next);
-// 	}
-// 	alert(list.value);
-// }
-
-printReverseList(list);
+alert(sum(1)(2));
+alert(sum(5)(-1));
