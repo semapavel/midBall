8365cc3b41b73c8acbbb9cfb3edb86a0bb438783
semapavel <semapavel89@gmail.com>
2022-06-25 00:33:35 +0500

    first commit

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-let dictionary = Object.create(null);
-Object.defineProperty(dictionary, "toString", {
-	value: function () {
-	return Object.keys(this).join(",");
-	},
-});
+class Clock {
+	constructor({ template }){
+		this.template=template;
+	}
 
-// ваш код, который добавляет метод dictionary.toString
+	render() {
+		let date = new Date();
 
-// добавляем немного данных
-dictionary.apple = "Apple";
-dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
+		let hours = date.getHours();
+		if (hours < 10) hours = '0' + hours;
 
-// только apple и __proto__ выведены в цикле
-for(let key in dictionary) {
-  alert(key); // "apple", затем "__proto__"
-}
+		let mins = date.getMinutes();
+		if (mins < 10) mins = '0' + mins;
 
-// ваш метод toString в действии
-alert(dictionary); // "apple,__proto__"
+		let secs = date.getSeconds();
+		if (secs < 10) secs = '0' + secs;
+		
+		let output = this.template
+		.replace('h', hours)
+		.replace('m', mins)
+		.replace('s', secs);
+		
+		console.log(output);
+	}
+	
+	stop() {
+		clearInterval(this.timer);
+	};
+	
+	start() {
+		this.render();
+		this.timer = setInterval(()=>this.render(), 1000);
+	};
+ 
+ }
+ 
+ let clock = new Clock({template: 'h:m:s'});
+ clock.start();
+
