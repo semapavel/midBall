a0fe9ec795ab35484da581e168685393bd20fe4b
semapavel <semapavel89@gmail.com>
2022-06-25 13:27:14 +0500

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function clear(elem) {
-	while (elem.firstChild) {
-		elem.firstChild.remove();
+let ul = document.createElement('ul');
+document.body.append(ul);
+
+while (true) {
+	let data = prompt("Введите текст для элемента списка", "");
+	if (!data) {
+		break
 	}
- }
- clear(elem);
+	let li = document.createElement('li');
+	li.textContent = data;
+	ul.append(li);
+}
