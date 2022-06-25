6f8ddd9a04589a8a8a57171185ed2bddd9d04bcb
semapavel <semapavel89@gmail.com>
2022-06-25 14:13:02 +0500

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-one.insertAdjacentHTML('afterend', '<li>2</li><li>3</li>');
+let sortedRows = Array.from(table.rows)
+  .slice(1)
+  .sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1);
+
+table.tBodies[0].append(...sortedRows);
