d70953faf59b4757025249077871d0bf1c88f076
semapavel <semapavel89@gmail.com>
2022-06-25 11:42:56 +0500

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-class FormatError extends SyntaxError{
-	constructor(message){
-		super(message);
-		this.name='FormatError';
-	}
-}
-
-let err = new FormatError("ошибка форматирования");
-
-alert( err.message ); // ошибка форматирования
-alert( err.name ); // FormatError
-alert( err.stack ); // stack
-
-alert( err instanceof FormatError ); // true
-alert( err instanceof SyntaxError ); // true (потому что наследует от SyntaxError)
+console.log(document.body.firstElementChild);
+console.log(document.body.children[1]);
+console.log(document.body.children[1].lastElementChild);
