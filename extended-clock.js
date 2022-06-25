66e3a10bfecf94d1a0c06daa09b871be17c93fb9
semapavel <semapavel89@gmail.com>
2022-06-25 10:55:28 +0500

    first commit

--- extended-clock.js
+++ extended-clock.js
@@ -1,13 +1,13 @@
-class ExtendedClock extends Clock {
-	constructor(options) {
-		super(options);
-		let { precision=1000 } = options;
-		this.precision = precision;
-	}
+// class ExtendedClock extends Clock {
+// 	constructor(options) {
+// 		super(options);
+// 		let { precision=1000 } = options;
+// 		this.precision = precision;
+// 	}
 	
-	start() {
-		this.render();
-		this.timer = setInterval(() => this.render(), this.precision);
-	}
-}
+// 	start() {
+// 		this.render();
+// 		this.timer = setInterval(() => this.render(), this.precision);
+// 	}
+// }
  
--- script.js
+++ script.js
@@ -1,36 +1,15 @@
-class Clock {
-	constructor({ template }){
-		this.template=template;
+class FormatError extends SyntaxError{
+	constructor(message){
+		super(message);
+		this.name='FormatError';
 	}
+}
 
-	render() {
-		let date = new Date();
+let err = new FormatError("ошибка форматирования");
 
-		let hours = date.getHours();
-		if (hours < 10) hours = '0' + hours;
+alert( err.message ); // ошибка форматирования
+alert( err.name ); // FormatError
+alert( err.stack ); // stack
 
-		let mins = date.getMinutes();
-		if (mins < 10) mins = '0' + mins;
-
-		let secs = date.getSeconds();
-		if (secs < 10) secs = '0' + secs;
-		
-		let output = this.template
-		.replace('h', hours)
-		.replace('m', mins)
-		.replace('s', secs);
-		
-		console.log(output);
-	}
-	
-	stop() {
-		clearInterval(this.timer);
-	};
-	
-	start() {
-		this.render();
-		this.timer = setInterval(()=>this.render(), 1000);
-	};
- 
- }
-
+alert( err instanceof FormatError ); // true
+alert( err instanceof SyntaxError ); // true (потому что наследует от SyntaxError)
