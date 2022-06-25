6a437ec970204afc3a6dafe39f75bc7c9c756fac
semapavel <semapavel89@gmail.com>
2022-06-25 13:52:41 +0500

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-let lis = document.getElementsByTagName('li');
+let timerId;
 
-for (let li of lis) {
-	let count = li.getElementsByTagName('li').length;
-	if (!count) continue;
-	li.firstChild.data += ' [' + count + ']';
+function update() {
+	let clock = document.getElementById('clock');
+	let date = new Date();
+	
+	let hours = date.getHours();
+	if (hours < 10) hours = '0' + hours;
+	clock.children[0].innerHTML = hours;
+	
+	let minutes = date.getMinutes();
+	if (minutes < 10) minutes = '0' + minutes;
+	clock.children[1].innerHTML = minutes;
+	
+	let seconds = date.getSeconds();
+	if (seconds < 10) seconds = '0' + seconds;
+	clock.children[2].innerHTML = seconds;
+}
+
+function clockStart() {
+	if (!timerId) {
+		timerId = setInterval(update, 1000);
+	}
+	update();
+}
+
+function clockStop() {
+	clearInterval(timerId);
+	timerId = null;
 }
