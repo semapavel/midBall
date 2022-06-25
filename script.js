c1ed97e756bd090339465343850426f5c5f89840
semapavel <semapavel89@gmail.com>
2022-06-25 14:41:39 +0500

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-let div = document.createElement('div');
-
-div.style.overflowY = 'scroll';
-div.style.width = '50px';
-div.style.height = '50px';
-
-document.body.append(div);
-let scrollWidth = div.offsetWidth - div.clientWidth;
-
-div.remove();
-
-alert(scrollWidth);
+ball.style.left = Math.round(field.clientWidth / 2 - ball.offsetWidth / 2) + 'px'
+ball.style.top = Math.round(field.clientHeight / 2 - ball.offsetHeight / 2) + 'px'
