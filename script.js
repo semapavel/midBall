1ffe3d3d47e9adb9d95c611cf6fcd89564e79385
semapavel <semapavel89@gmail.com>
2022-06-25 14:56:21 +0500

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-ball.style.left = Math.round(field.clientWidth / 2 - ball.offsetWidth / 2) + 'px'
-ball.style.top = Math.round(field.clientHeight / 2 - ball.offsetHeight / 2) + 'px'
+document.onclick = function(e) { // показывает координаты точки клика
+	coords.innerHTML = e.clientX + ':' + e.clientY;
+};
+
+let fieldCoords = field.getBoundingClientRect();
+
+let answer = [
+	[
+		fieldCoords.left,
+		fieldCoords.top
+	],
+	[
+		fieldCoords.right,
+		fieldCoords.bottom
+	],
+	[
+		fieldCoords.left + field.clientLeft,
+		fieldCoords.top + field.clientTop
+	],
+	[
+		fieldCoords.left + field.clientLeft + field.clientWidth,
+		fieldCoords.top + field.clientTop + field.clientHeight
+	]
+];
+
+alert(answer.join('  '));
