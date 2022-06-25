57e59fd4c7cbba81c8f0803cb8ee09bc23cad722
semapavel <semapavel89@gmail.com>
2022-06-25 14:03:02 +0500

    first commit

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-let timerId;
+function createCalendar(elem, year, month) {
+	let mon = month - 1;
+	let d = new Date(year, mon);
+	
+	let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
 
-function update() {
-	let clock = document.getElementById('clock');
-	let date = new Date();
-	
-	let hours = date.getHours();
-	if (hours < 10) hours = '0' + hours;
-	clock.children[0].innerHTML = hours;
-	
-	let minutes = date.getMinutes();
-	if (minutes < 10) minutes = '0' + minutes;
-	clock.children[1].innerHTML = minutes;
-	
-	let seconds = date.getSeconds();
-	if (seconds < 10) seconds = '0' + seconds;
-	clock.children[2].innerHTML = seconds;
-}
-
-function clockStart() {
-	if (!timerId) {
-		timerId = setInterval(update, 1000);
+	for (let i = 0; i < getDay(d); i++) {
+		table += '<td></td>';
 	}
-	update();
+
+
+	while (d.getMonth() == mon) {
+		table += '<td>' + d.getDate() + '</td>';
+		if (getDay(d) % 7 == 6) {
+			table += '</tr><tr>';
+		}
+		
+		d.setDate(d.getDate() + 1);
+	}
+
+	if (getDay(d) != 0) {
+		for (let i = getDay(d); i < 7; i++) {
+			table += '<td></td>';
+		}
+	}
+
+	table += '</tr></table>';
+	elem.innerHTML = table;
 }
 
-function clockStop() {
-	clearInterval(timerId);
-	timerId = null;
+function getDay(date) {
+	let day = date.getDay();
+	if (day == 0) day = 7;
+	return day - 1;
 }
+
+createCalendar(calendar, 2012, 9);
